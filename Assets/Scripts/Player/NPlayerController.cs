26e9af47934f4833fdf3a553e4dfcbaa29c11693
10690037 <10690037@student.bpc.ac.uk>
2023-11-30 11:24:44 +0000

    Imported assets and worked on Main Menu script

--- Assets/Scripts/Player/NPlayerController.cs
+++ Assets/Scripts/Player/NPlayerController.cs
@@ -1,41 +1,72 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.UI;
 
 public class NPlayerController : MonoBehaviour
 {
+    // main menu and associated fields
     [SerializeField] private GameObject MainPanel;
+    [SerializeField] private GameObject SettingsPanel;
     [SerializeField] private Button PlayButton;
     [SerializeField] private Button ContinueButton;
     [SerializeField] private Button SettingsButton;
+    [SerializeField] private Button SettingsReturnButton;
     [SerializeField] private Button QuitButton;
 
-    settingsOpen = false
+    // in-game information
+    [SerializeField] private GameObject InformationPanel;
+    [SerializeField] private Text MoneyText;
     
-    // Start is called before the first frame update
+    int money = 0;
+    bool settingsOpen = false;
+    
     void Start(){
+        MainPanel.SetActive(true);
+        SettingsPanel.SetActive(false);
+        InformationPanel.SetActive(false);
         PlayButton.onClick.AddListener(PlayClicked);
         ContinueButton.onClick.AddListener(PlayClicked);
-        SettingsButton.onClick.AddListener(PlayClicked);
+        SettingsButton.onClick.AddListener(SettingsClicked);
+        SettingsReturnButton.onClick.AddListener(SettingsClicked);
         QuitButton.onClick.AddListener(QuitGame);
+        string sMoney = money.ToString();
+        MoneyText.text = "£"+sMoney;
     }
 
     void PlayClicked(){
         MainPanel.SetActive(false);
+        InformationPanel.SetActive(true);
+        SetCursorState(true);
     }
 
     void SettingsClicked(){
-
+        if(settingsOpen == false){
+            settingsOpen = true;
+            MainPanel.SetActive(false);
+            SettingsPanel.SetActive(true);
+        } else {
+            settingsOpen = false;
+            MainPanel.SetActive(true);
+            SettingsPanel.SetActive(false);
+        }
     }
 
     public void QuitGame(){
+        UnityEditor.EditorApplication.isPlaying = false;
         Application.Quit();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    private void OnApplicationFocus(bool hasFocus){
+        SetCursorState(true);
+    }
+
+    private void SetCursorState(bool newState){
+        Cursor.lockState = newState ? CursorLockMode.Locked : CursorLockMode.None;
+    }
+
+    void Update(){
         
     }
 }
