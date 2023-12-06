8f47fb58bcdfad9570d1a4bcb44a38aca700df38
10690037 <10690037@student.bpc.ac.uk>
2023-12-06 09:32:59 +0000

    h

--- Assets/Scripts/GoldReward.cs
+++ Assets/Scripts/GoldReward.cs
@@ -3,11 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GoldReward : MonoBehaviour, IInteractable {
-    [SerializeField] public text MoneyText;
+    // [SerializeField] public text MoneyText;
     public void Interact(){
-        string money = MoneyText.text;
-        money = money.Substring(1,1);
-        Debug.Log(money);
+        // string money = MoneyText.text;
+        // money = money.Substring(1,1);
+        // Debug.Log(money);
         Destroy(gameObject);
     }
 }
